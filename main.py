49c1bead63c09cb4a1c05054af7ccb069a1b61e6
Giridharvirat <63089153+Giridharvirat@users.noreply.github.com>
2021-11-20 06:58:32 +0000

    ticketing

--- main.py
+++ main.py
@@ -3,7 +3,13 @@ height = int(input("What is your height in cm? "))
 
 if height >= 120 :
  print("you can ride")
-
+ age = int(input("what is your age?"))
+ if age <=12 :
+   print("please pay $5.")
+ elif age <= 18 :
+   print("please pay $7.")
+ else :
+   print("please pay $8.")   
 else :
  print("you can't ride")
 
